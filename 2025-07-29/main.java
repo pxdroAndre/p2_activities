f02c2036009205efc132e37c7644a806284c4723
André <139287902+pxdroAndre@users.noreply.github.com>
2025-07-30 18:02:34 -0300

    Update main.java

--- 2025-07-29/main.java
+++ 2025-07-29/main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Main
 {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+	public static void main(String[] args) 
+	{
+	    
+	    
+		Scanner scan = new Scanner(System.in);
+		String Name = "bla";
+		double price = 20.3;
+		int qtt = 4;
+		Product prod = new Product(Name, price, qtt);
+	
+		System.out.println(prod.Name);
 	}
 }
+
