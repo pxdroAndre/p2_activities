296fe99a8a1ea4ea40c014d0da302ad6519a08fc
André <139287902+pxdroAndre@users.noreply.github.com>
2025-07-31 16:23:49 -0300

    scanning and object working

--- 2025-07-29/main.java
+++ 2025-07-29/main.java
@@ -4,15 +4,18 @@ public class Main
 {
 	public static void main(String[] args) 
 	{
-	    
-	    
+	    System.out.println("Enter the product data");
 		Scanner scan = new Scanner(System.in);
-		String Name = "bla";
-		double price = 20.3;
-		int qtt = 4;
+		System.out.print("Enter the product name:");
+		String Name = scan.nextLine();
+		
+		System.out.print("Enter the product price: $");
+		double price = scan.nextDouble();
+		
+		System.out.print("Enter the product quantity:");
+		int qtt = scan.nextInt();
+		
 		Product prod = new Product(Name, price, qtt);
 	
-		System.out.println(prod.Name);
 	}
 }
-
