76f947364b73b3723a68ae9196033b57b05af580
André <139287902+pxdroAndre@users.noreply.github.com>
2025-07-31 16:51:15 -0300

    showing the value working

--- 2025-07-29/main.java
+++ 2025-07-29/main.java
@@ -2,20 +2,24 @@ import java.util.Scanner;
 
 public class Main
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
-	    System.out.println("Enter the product data");
+		System.out.println("Enter the product data");
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter the product name:");
 		String Name = scan.nextLine();
-		
+
 		System.out.print("Enter the product price: $");
 		double price = scan.nextDouble();
-		
+
 		System.out.print("Enter the product quantity:");
 		int qtt = scan.nextInt();
-		
+
 		Product prod = new Product(Name, price, qtt);
-	
+    
+        double total = prod.quantity * prod.price;
+  
+        System.out.printf("Product %s has %d in stock, and the total amount of: $%.2f\n", prod.Name, prod.quantity, total);
+
 	}
 }
