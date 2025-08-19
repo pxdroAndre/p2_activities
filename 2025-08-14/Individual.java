222a859e8fedda99cb8e2b04aae5ec3af9055b02
André <139287902+pxdroAndre@users.noreply.github.com>
2025-08-19 18:03:34 -0300

    Update Individual.java

--- 2025-08-14/Individual.java
+++ 2025-08-14/Individual.java
@@ -1,9 +1,34 @@
-public class Individual extends TaxPayer
+import java.util.Scanner;
+
+public abstract class TaxPayer
 {
-    private double healthExpenditures;
-    
-    public void getHealth()
-    {
-        
+    private String name;
+    private double annualIncome;
+    private double taxes;
+
+    abstract void tax();
+
+    public final String getName() {
+        return name;
     }
+    public final void setName(String name) {
+        this.name = name;
+    }
+
+    public final double getAnnualIncome() {
+        return annualIncome;
+    }
+
+    public final void setAnnualIncome(double annualIncome) {
+        this.annualIncome = annualIncome;
+    }
+
+    public final double getTaxes() {
+        return taxes;
+    }
+
+    public final void setTaxes(double taxes) {
+        this.taxes = taxes;
+    }
+
 }
