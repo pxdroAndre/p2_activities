3be03bfdd6c8c9c9fee180bfd41b3101cc3f5748
André <139287902+pxdroAndre@users.noreply.github.com>
2025-08-19 18:02:47 -0300

    Update Main.java with new logic

--- 2025-08-14/Main.java
+++ 2025-08-14/Main.java
@@ -1,24 +1,49 @@
+import java.util.Objects;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main
 {
-	public static void main(String[] args) 
-	{
-	    Scanner scanf = new Scanner(System.in);
-	    System.out.print("Enter the number of tax payers: ");
-	    int counter = scanf.nextInt();
-	    
-	    for (int i = 1; i <= counter; i++)
-	    {
-	        System.out.println("Tax payer #" + i + " data:");
-	        System.out.print("Individual or company? (i/c) ");
-	        String ans = scan.nextLine();
-	        if (ans == "i")
-	        {
-	            Individual person = new Individual();
-	            person.getData(scan);
-	        }
-	    }
-	    
-	}
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number of tax payers: ");
+        int counter = scan.nextInt();
+
+        for (int i = 1; i <= counter; i++)
+        {
+            System.out.println("Tax payer #" + i + " data:");
+            System.out.print("Individual or company? (i/c) ");
+            String ans = scan.nextLine();
+            List <TaxPayer> taxPayerList = new ArrayList<TaxPayer>();
+
+            if (Objects.equals(ans, "i"))
+            {
+                Individual person = new Individual();
+                String name = scan.nextLine();
+                double income = scan.nextDouble();
+                double expenditures = scan.nextDouble();
+                person.setName(name);
+                person.setAnnualIncome(income);
+                person.setHealthExpenditures(expenditures);
+                person.tax();
+                taxPayerList.add(person);
+
+            }
+            else
+            {
+                Company company = new Company();
+                String name = scan.nextLine();
+                double income = scan.nextDouble();
+                int employees = scan.nextInt();
+                company.setName(name);
+                company.setAnnualIncome(income);
+                company.setNumEmployees(employees);
+                company.tax();
+                taxPayerList.add(company);
+            }
+        }
+
+    }
 }
