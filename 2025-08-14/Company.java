7b241ae90859108de37e986e403651fee174e96d
André <139287902+pxdroAndre@users.noreply.github.com>
2025-08-19 18:03:47 -0300

    Update Company.java

--- 2025-08-14/Company.java
+++ 2025-08-14/Company.java
@@ -1,5 +1,29 @@
-public class Company
-  {
+public class Company extends TaxPayer
+{
+    private int numEmployees;
+
+    @Override
+    void tax()
+    {
+        double tax;
+        if (this.getNumEmployees() <= 10)
+        {
+            tax = (this.getAnnualIncome() * 0.16);
+        }
+        else
+        {
+            tax = (this.getAnnualIncome() * 0.14);
+        }
+        this.setTaxes(tax);
+    }
+
+    public int getNumEmployees() {
+        return numEmployees;
+    }
+
+    public void setNumEmployees(int numEmployees) {
+        this.numEmployees = numEmployees;
+    }
 
 
-  }  
+}
