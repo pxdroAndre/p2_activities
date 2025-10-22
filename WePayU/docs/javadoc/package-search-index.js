dbcc09ebbad3f794ac65c9235bea76559203918f
pxdroAndre <andreneto0406@gmail.com>
2025-10-22 11:59:30 -0300

    relatorio updated

--- WePayU/docs/javadoc/package-search-index.js
+++ WePayU/docs/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"br.ufal.ic.p2.wepayu"},{"l":"br.ufal.ic.p2.wepayu.Exception"},{"l":"br.ufal.ic.p2.wepayu.models"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"br.ufal.ic.p2.wepayu"},{"l":"br.ufal.ic.p2.wepayu.commands"},{"l":"br.ufal.ic.p2.wepayu.Exception"},{"l":"br.ufal.ic.p2.wepayu.models"},{"l":"br.ufal.ic.p2.wepayu.utilities"}];updateSearchResults();
